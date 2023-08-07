0a2df18fa538bb0670b3caaae7bdc63c495bc1e2
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-08-07 17:24:30 +0300

    FIX: Code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './screens/RegistrationScreen';
